f830b0baf96542e3096127909d9454b1fe3a9edc
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-05-15 07:58:43 -0400

    read all jar files from a directory

--- getjarstats/Program.cs
+++ getjarstats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 
 namespace getjarstats
@@ -9,6 +10,16 @@ namespace getjarstats
         static void Main(string[] args)
         {
             Console.WriteLine("*** begin ***");
+            string directoryName = @"/Users/mario/Projects/getjarstats/getjarstats/data";
+            string[] fileNames = Directory.GetFiles(directoryName);
+            foreach (string fileName in fileNames)
+            {
+                if (fileName.EndsWith(".jar", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(fileName);
+                }
+            }
+            return;
             string zipPath = @"/Users/mario/Projects/getjarstats/getjarstats/data/junit-platform-console-standalone-1.6.2.jar";
             List<string> classes = new List<string>();
             using (ZipArchive archive = ZipFile.OpenRead(zipPath))
