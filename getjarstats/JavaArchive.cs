5590d830c8142b5a4bfe81450dfe4c2f3a510ca5
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-05-16 13:26:06 -0400

    add JavaArchive

---
+++ getjarstats/JavaArchive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace getjarstats
+{
+    public class JavaArchive
+    {
+        private string fileName;
+        public JavaArchive(JarFile jarFile)
+        {
+            fileName = jarFile.JarFileName;
+        }
+
+        public string FileName { get => fileName; }
+    }
+}
--- getjarstats/Program.cs
+++ getjarstats/Program.cs
@@ -10,18 +10,31 @@ namespace getjarstats
         static void Main(string[] args)
         {
             Console.WriteLine("*** begin ***");
+            Console.WriteLine("*** begin ***");
+            Console.WriteLine("*** begin ***");
             var directory = @"/Users/mario/Projects/getjarstats/getjarstats/data";
             var jarFiles = GetJarFiles(directory);
-            WriteJarFiles(jarFiles);
+            var javaArchives = new List<JavaArchive>();
+            foreach (var jarFile in jarFiles)
+            {
+                var javaArchive = new JavaArchive(jarFile);
+                javaArchives.Add(javaArchive);
+            }
+            WriteStatsJarFiles(jarFiles);
+            WriteStatsJavaArchives(javaArchives);
+            Console.WriteLine("*** end ***");
+            Console.WriteLine("*** end ***");
             Console.WriteLine("*** end ***");
         }
 
-        private static void WriteJarFiles(List<JarFile> jarFiles)
+        private static void WriteStatsJarFiles(List<JarFile> jarFiles)
         {
+            Console.WriteLine($"number of jar files: {jarFiles.Count}");
             foreach (var jarFile in jarFiles)
             {
                 Console.WriteLine($"file: {jarFile.JarFileName}");
                 var jarClasses = jarFile.JarClasses;
+                Console.WriteLine($"number of classes: {jarClasses.Count}");
                 int i = 0;
                 foreach (var jarClass in jarClasses)
                 {
@@ -32,7 +45,16 @@ namespace getjarstats
                         break;
                     }
                 }
-                Console.WriteLine($"number of classes found: {jarClasses.Count}");
+                Console.WriteLine("...");
+            }
+        }
+
+        private static void WriteStatsJavaArchives(List<JavaArchive> javaArchives)
+        {
+            Console.WriteLine($"number of java archives: {javaArchives.Count}");
+            foreach (var javaArchive in javaArchives)
+            {
+                Console.WriteLine($"archive: {javaArchive.FileName}");
             }
         }
 
