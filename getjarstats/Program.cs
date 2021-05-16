286521b146a5301d1bda23856e168722e71ea5d8
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-05-15 22:50:01 -0400

    implement GetJarFiles and GetJarClasses

--- getjarstats/Program.cs
+++ getjarstats/Program.cs
@@ -10,31 +10,60 @@ namespace getjarstats
         static void Main(string[] args)
         {
             Console.WriteLine("*** begin ***");
-            string directoryName = @"/Users/mario/Projects/getjarstats/getjarstats/data";
-            string[] fileNames = Directory.GetFiles(directoryName);
-            foreach (string fileName in fileNames)
+            var directory = @"/Users/mario/Projects/getjarstats/getjarstats/data";
+            var jarFiles = GetJarFiles(directory);
+            foreach (var jarFile in jarFiles)
             {
-                if (fileName.EndsWith(".jar", StringComparison.OrdinalIgnoreCase))
+                var jarFileName = Path.GetFileName(jarFile);
+                Console.WriteLine($"file: {jarFileName}");
+                var jarClasses = GetJarClasses(jarFile);
+                int i = 0;
+                foreach (var jarClass in jarClasses)
                 {
-                    Console.WriteLine(fileName);
-                }
-            }
-            return;
-            string zipPath = @"/Users/mario/Projects/getjarstats/getjarstats/data/junit-platform-console-standalone-1.6.2.jar";
-            List<string> classes = new List<string>();
-            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
-            {
-                Console.WriteLine("archive has been opened");
-                foreach (ZipArchiveEntry entry in archive.Entries)
-                {
-                    if (entry.FullName.EndsWith(".class"))
+                    i++;
+                    Console.WriteLine($"class: {jarClass}");
+                    if (i >= 5)
                     {
-                        classes.Add(entry.FullName);
+                        break;
+                    }
+                }
+                Console.WriteLine($"number of classes found: {jarClasses.Count}");
+            }
+            Console.WriteLine("*** end ***");
+        }
+
+        private static List<string> GetJarClasses(string jarFile)
+        {
+            var jarClasses = new List<string>();
+            using (var archive = ZipFile.OpenRead(jarFile))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    var classExtension = ".class";
+                    var jarClass = entry.FullName;
+                    if (jarClass.EndsWith(classExtension))
+                    {
+                        jarClass = jarClass.Substring(0, jarClass.Length - classExtension.Length);
+                        jarClass = jarClass.Replace("/", ".");
+                        jarClasses.Add(jarClass);
                     }
                 }
             }
-            Console.WriteLine($"classed found: {classes.Count}");
-            Console.WriteLine("*** end ***");
+            return jarClasses;
+        }
+
+        private static List<string> GetJarFiles(string directory)
+        {
+            var jarFiles = new List<string>();
+            var files = Directory.GetFiles(directory);
+            foreach (var file in files)
+            {
+                if (file.EndsWith(".jar", StringComparison.OrdinalIgnoreCase))
+                {
+                    jarFiles.Add(file);
+                }
+            }
+            return jarFiles;
         }
     }
 }
