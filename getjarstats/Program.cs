276f92ecae61b92ed7689c947c62218a87b481b5
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-05-22 23:35:07 -0400

    works with only an archives dictionnary

--- getjarstats/Program.cs
+++ getjarstats/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace getjarstats
 {
@@ -11,9 +12,10 @@ namespace getjarstats
         {
             var directory = @"/Users/mario/Projects/getjarstats/getjarstats/data";
             var archivesDictionary = GetArchivesDictionary(directory);
-            var classesDictionary = GetClassesDictionary(archivesDictionary);
-            var archivesWithDuplicateClassesDictionary = GetArchivesWithDuplicateClassesDictionary(classesDictionary);
-            WriteStats(archivesDictionary, classesDictionary, archivesWithDuplicateClassesDictionary);
+            WriteStats(archivesDictionary);
+            // var classesDictionary = GetClassesDictionary(archivesDictionary);
+            // var archivesWithDuplicateClassesDictionary = GetArchivesWithDuplicateClassesDictionary(classesDictionary);
+            // WriteStats(archivesDictionary, classesDictionary, archivesWithDuplicateClassesDictionary);
         }
 
         private static Dictionary<string, List<string>> GetArchivesDictionary(string directory)
@@ -51,67 +53,35 @@ namespace getjarstats
             }
             return classes;
         }
-        private static Dictionary<string, List<string>> GetClassesDictionary(Dictionary<string, List<string>> archivesDictionary)
+        private static void WriteStats(Dictionary<string, List<string>> archivesDictionary)
         {
-            var classesDictionary = new Dictionary<string, List<string>>();
-            foreach (var archivesDictionaryItem in archivesDictionary)
+            foreach (var archivesDictionaryFirstItem in archivesDictionary)
             {
-                var archive = archivesDictionaryItem.Key;
-                var classes = archivesDictionaryItem.Value;
-                foreach (var className in classes)
+                var archiveFirst = archivesDictionaryFirstItem.Key;
+                var classesFirst = archivesDictionaryFirstItem.Value;
+                foreach (var archivesDictionarySecondItem in archivesDictionary)
                 {
-                    if (!classesDictionary.ContainsKey(className))
+                    var archiveSecond = archivesDictionarySecondItem.Key;
+                    var classesSecond = archivesDictionarySecondItem.Value;
+                    if (string.Compare(archiveSecond, archiveFirst) > 0) // archiveSecond > archiveFirst
                     {
-                        classesDictionary.Add(className, new List<string>());
-                    }
-                    classesDictionary[className].Add(archive);
-                }
-            }
-            return classesDictionary;
-        }
-        private static Dictionary<string, List<string>> GetArchivesWithDuplicateClassesDictionary(Dictionary<string, List<string>> classesDictionary)
-        {
-            var archivesWithDuplicateClassesDictionary = new Dictionary<string, List<string>>();
-            foreach (var classesDictionaryItem in classesDictionary)
-            {
-                var className = classesDictionaryItem.Key;
-                var archives = classesDictionaryItem.Value;
-                if (archives.Count > 1)
-                {
-                    var archiveNames = string.Join(",", archives);
-                    if (!archivesWithDuplicateClassesDictionary.ContainsKey(archiveNames))
-                    {
-                        archivesWithDuplicateClassesDictionary.Add(archiveNames, new List<string>());
-                    }
-                    archivesWithDuplicateClassesDictionary[archiveNames].Add(className);
-                }
-            }
-            return archivesWithDuplicateClassesDictionary;
-        }
-        private static void WriteStats(Dictionary<string, List<string>> archivesDictionary, Dictionary<string, List<string>> classesDictionary, Dictionary<string, List<string>> archivesWithDuplicateClassesDictionary)
-        {
-            Console.WriteLine($"Found {classesDictionary.Count} classes in {archivesDictionary.Count} archives.");
-            int count = 0;
-            var duplicateArchivesHasSet = new HashSet<string>();
-            foreach (var classesDictionaryItem in classesDictionary)
-            {
-                var archives = classesDictionaryItem.Value;
-                if (archives.Count > 1)
-                {
-                    count++;
-                    foreach (var archive in archives)
-                    {
-                        duplicateArchivesHasSet.Add(archive);
+                        var duplicateClasses = classesFirst.Intersect(classesSecond).ToList();
+                        if (duplicateClasses.Count > 0)
+                        {
+                            var percentageFirst =  duplicateClasses.Count * 100 / classesFirst.Count;
+                            var percentageSecond =  duplicateClasses.Count * 100 / classesSecond.Count;
+                            Console.WriteLine($"Found {duplicateClasses.Count:N0} duplicate classes between {archiveFirst} and {archiveSecond}.");
+                            Console.WriteLine($"Those duplicate classes represent {percentageFirst}% of the {classesFirst.Count:N0} classes in {archiveFirst} and {percentageSecond}% of the {classesSecond.Count:N0} classes in {archiveSecond}.");
+                            Console.WriteLine($"One such duplicate class is {duplicateClasses[0]}.");
+                            // TODO: display no duplicate found message when no duplicate
+                            // TODO: sort archives by name (ie don't use dictionary)
+                            // TODO: randomly select a duplicate class
+                            // TODO: singular vs plural - Found x duplicate classes vs Found 1 duplicate class
+                            // TODO: singular vs plural - Those duplicate classes vs This duplicate class
+                            // TODO: singular vs plural - of the x classes vs of the 1 class
+                        }
                     }
                 }
-            }
-            Console.WriteLine($"Found {duplicateArchivesHasSet.Count} archives with at least one class that is in another archive.");
-            Console.WriteLine($"Found {count} classes that are in more than 1 archive.");
-            foreach (var archivesWithDuplicateClassesDictionaryItem in archivesWithDuplicateClassesDictionary)
-            {
-                var archivesNames = archivesWithDuplicateClassesDictionaryItem.Key;
-                var classes = archivesWithDuplicateClassesDictionaryItem.Value;
-                Console.WriteLine($"Found {classes.Count} classes that are in each of those archives: {archivesNames}. One such class is {classes[0]}.");
             }
         }
     }
